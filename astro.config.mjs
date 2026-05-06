8dda70d28dbae3afe4b934d769f96121a952b265
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-05-06 14:59:51 +1000

    feat: add custom domain for Resend

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321/",
+  site: "https://markdown-editor.net/",
 
   image: {
     responsiveStyles: true,
